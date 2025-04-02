4e6e6fdd661a16bbe98d973fbb6dd35afa6b5d2a
ph <chenyi.ph@outlook.com>
2025-04-02 21:08:41 +0800

    use callPackage to overlay

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -2,13 +2,9 @@
   nixpkgs,
   aikadm-frontend,
   ...
-}: {
-  default = final: _prev: let
-    packages = import ./pkgs.nix {
-      inherit nixpkgs aikadm-frontend;
-      system = final.system;
-    };
-  in {
-    aikadm = packages;
+}:
+{
+  default = _: prev: {
+    aikadm = prev.callPackage (import ./pkgs.nix) { inherit aikadm-frontend nixpkgs; };
   };
 }
